1c4f8bf328307ea1e9d8c76c1e89581793ccb4a7
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-07-20 23:29:03 -0500

    update instalar

--- instalar.sh
+++ instalar.sh
@@ -6,4 +6,6 @@ apt-get install valgrind vim-gnome dos2unix -y
 apt-get install python python-dev libxml2-dev libxslt-dev -y
 apt-get install tmux ack-grep astyle -y
 apt-get install dpkg-dev -y
+apt-get install python-pip lcov coverage -y
 apt-get autoremove -qq -y
+pip install --user cpp-coveralls --upgrade
