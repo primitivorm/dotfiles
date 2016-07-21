59e95cc0034e1ecf71c7dcce570d2b177b70d23d
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-07-20 21:09:56 -0500

    update files

--- instalar.sh
+++ instalar.sh
@@ -5,4 +5,5 @@ apt-get install build-essential gcc g++ git make cmake flex bison clang -y
 apt-get install valgrind vim-gnome dos2unix -y
 apt-get install python python-dev libxml2-dev libxslt-dev -y
 apt-get install tmux ack-grep astyle -y
+apt-get install dpkg-dev -y
 apt-get autoremove -qq -y
