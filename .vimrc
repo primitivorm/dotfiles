f31d49d6dbbbfb4f34471965f240af2c030fb330
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-08-16 11:21:59 -0500

    update .tmuxconf

--- .vimrc
+++ .vimrc
@@ -99,7 +99,7 @@ set nocompatible " be iMproved
 call vundle#begin('~/.vim/bundle/')
 "" let Vundle manage Vundle
 "" required!
-Plugin 'Align'
+Plugin 'primitivorm/Align'
 Plugin 'L9'
 Plugin 'SirVer/ultisnips'
 Plugin 'Valloric/YouCompleteMe'
--- instalar.sh
+++ instalar.sh
@@ -1,12 +1,12 @@
 #install packages
 echo "instalando paquetes..."
 apt-get update -qq -y
-apt-get install build-essential gcc g++ 
+apt-get install aptitude build-essential gcc g++ 
 apt-get install git make cmake flex bison clang llvm -y
 apt-get install valgrind vim-gnome dos2unix -y
 apt-get install python python-dev libxml2-dev libxslt-dev -y
 apt-get install tmux ack-grep astyle -y
 apt-get install dpkg-dev -y
-apt-get install python-pip lcov coverage -y
+apt-get install python-pip lcov -y
 apt-get autoremove -qq -y
 pip install --user cpp-coveralls --upgrade
