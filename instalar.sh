38dc3c7d573654322e345c1d3da4237d37b6ced6
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-08-30 20:43:54 -0500

    update files

--- instalar.sh
+++ instalar.sh
@@ -4,8 +4,8 @@ apt-get update -qq -y
 apt-get install build-essential gcc g++ git make cmake flex bison clang -y
 apt-get install valgrind vim-gnome dos2unix -y
 apt-get install python python-dev libxml2-dev libxslt-dev -y
-apt-get install tmux ack-grep astyle -y
+apt-get install tmux ack-grep astyle exuberant-ctags -y
 apt-get install dpkg-dev -y
-apt-get install python-pip lcov coverage -y
+apt-get install python-pip lcov coverage  -y
 apt-get autoremove -qq -y
 pip install --user cpp-coveralls --upgrade
