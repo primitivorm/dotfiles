c62d30ec38dfc259c38a70319a04b7a6b31043a0
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-10-06 17:06:48 -0500

    uf

--- instalar.sh
+++ instalar.sh
@@ -7,7 +7,7 @@ apt-get install valgrind vim-gnome dos2unix -y
 apt-get install python python-dev libxml2-dev libxslt-dev -y
 apt-get install tmux ack-grep astyle -y
 apt-get install dpkg-dev libgnome-keyring-dev -y
-apt-get install python-pip lcov -y
+apt-get install python-pip lcov npm ruby-coffee-script nodejs -y
 apt-get install default-jre default-jdk -y
 apt-get install lua5.2 liblua5.2-dev -y
 apt-get install gnome-system-monitor -y
@@ -15,10 +15,11 @@ apt-get install libjansson-dev libcurl4-openssl-dev curl -y
 apt-get autoremove -qq -y
 pip install --user cpp-coveralls --upgrade
 
+gem install grammars
+gem install pry
+
 #########################################################
 #install gnome-keyring
 #########################################################
 cd /usr/share/doc/git/contrib/credential/gnome-keyring
 make
-
-sudo cp -f ~/dotfiles/usr/share/nano/latino.nanorc /usr/share/nano/
