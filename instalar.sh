31b4705358378bce78fe91881c4545d8cd246a93
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2017-03-24 14:40:01 -0600

    uf

--- instalar.sh
+++ instalar.sh
@@ -13,7 +13,7 @@ apt-get install lua5.2 liblua5.2-dev meld -y
 apt-get install gnome-system-monitor clisp libgtk-3-dev -y
 apt-get install libjansson-dev libcurl4-openssl-dev curl -y
 apt-get install libgccjit-5-dev xd mono-xbuild -y
-apt-get install konsole dump gwhere -y
+apt-get install konsole dump gwhere gnuplot plotutils -y
 apt-get autoremove -qq -y
 
 #install pip packages
