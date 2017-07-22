5b90096948a9c04fe610e24052a00bdfb3a680f7
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2017-07-22 10:35:41 -0500

    uf

--- instalar.sh
+++ instalar.sh
@@ -42,8 +42,6 @@ make -j$(nproc)
 #########################################################
 if [ ! -d ~/src/vim ]; then
     echo "compilando e instalando vim..."
-=======
-if [ ! -d ~/src/vim ]; then 
     apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
