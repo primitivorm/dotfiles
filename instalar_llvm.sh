af9fdcc10b5990dc535dd11f2d669bf7117644ab
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2017-12-07 17:18:55 -0600

    uf

--- instalar_llvm.sh
+++ instalar_llvm.sh
@@ -1,5 +1,6 @@
 #https://llvm.org/docs/CMake.html
 #https://llvm.org/docs/HowToCrossCompileLLVM.html
+
 cd ~
 if [ ! -d ~/src ]; then
     mkdir src
@@ -16,20 +17,26 @@ cd ~/src/llvm-mirror/llvm/tools/clang/tools
 git clone --recursive https://github.com/llvm-mirror/clang-tools-extra
 cd ~/src/llvm-mirror/llvm/tools/
 git clone --recursive https://github.com/llvm-mirror/lld
-git clone --recursive https://github.com/llvm-mirror/polly
+#git clone --recursive https://github.com/llvm-mirror/polly
 cd ~/src/llvm-mirror/llvm/projects/
 git clone --recursive https://github.com/llvm-mirror/compiler-rt
-git clone --recursive https://github.com/llvm-mirror/openmp
+#git clone --recursive https://github.com/llvm-mirror/openmp
 git clone --recursive https://github.com/llvm-mirror/libcxx
 git clone --recursive https://github.com/llvm-mirror/libcxxabi
-git clone --recursive https://github.com/llvm-mirror/test-suite
+#git clone --recursive https://github.com/llvm-mirror/test-suite
 cd ~/src/llvm-mirror/
 if [ ! -d ~/src/llvm-mirror/build ]; then
     mkdir build
 fi
 cd ~/src/llvm-mirror/build
-CC='clang' CXX='clang++' cmake -G "Unix Makefiles" -DLLVM_BUILD_EXAMPLES=ON \
--DCLANG_BUILD_EXAMPLES=ON -DBUILD_SHARED_LIBS=ON -DLLVM_BUILD_LLVM_DYLIB=ON \
--DLLVM_ENABLE_PIC=False -DCMAKE_BUILD_TYPE=Release ../llvm/
+CC='clang' CXX='clang++' cmake -G "Unix Makefiles" \
+-DLLVM_BUILD_EXAMPLES=ON \
+-DCLANG_BUILD_EXAMPLES=ON \
+-DCMAKE_BUILD_TYPE=Release ../llvm/
+ 
+#-DCMAKE_INSTALL_PREFIX=/usr/bin
+#-DLLVM_TARGETS_TO_BUILD="X86"
+#-DBUILD_SHARED_LIBS=ON
+#-DLLVM_BUILD_LLVM_DYLIB=ON 
 make -j$(nproc)
 sudo make install
