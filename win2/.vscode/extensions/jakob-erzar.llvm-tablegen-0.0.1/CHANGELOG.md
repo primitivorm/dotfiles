a53f60d1a34f28fc66affade51e984b97403b263
PrimitivoRM <cibercafe_montero@hotmail.com>
2021-04-23 15:43:08 -0500

    se elimina folder win2

--- win2/.vscode/extensions/jakob-erzar.llvm-tablegen-0.0.1/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Change Log
-
-## [0.0.1] - 2018-04-27
-- Initial release, featuring ismple syntax highlighting.
--- win2/pshell/install_poshgit.ps1
+++
@@ -1,6 +0,0 @@
-# configure ExecutionPolicy
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-# Install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# Install posh-git
-choco install poshgit
